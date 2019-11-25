312fad8a05b9436987993d8d356d4d8c376f82b4
Guilherme Pereira <guuuilp@gmail.com>
2019-11-25 12:43:20 -0300

    Adding unit test for CharacterRepository

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,4 +1,4 @@
-const val kotlinVersion = "1.3.60"
+
 
 object BuildPlugins {
 
@@ -7,7 +7,7 @@ object BuildPlugins {
     }
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.buildToolsVersion}"
-    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${SharedVersions.kotlin}"
     const val androidApplication = "com.android.application"
     const val kotlinAndroid = "kotlin-android"
     const val kotlinAndroidExtensions = "kotlin-android-extensions"
@@ -33,6 +33,11 @@ object Modules {
     const val domain = ":domain"
 }
 
+private object SharedVersions {
+    const val coroutines = "1.3.2"
+    const val kotlin = "1.3.60"
+}
+
 object Libraries {
     private object Versions {
         const val appCompat = "1.0.0-beta01"
@@ -45,7 +50,7 @@ object Libraries {
         const val moshi = "1.9.2"
     }
 
-    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${SharedVersions.kotlin}"
     const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
     const val constraintLayout =
         "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
@@ -60,6 +65,7 @@ object Libraries {
     const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
     const val moshi = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
     const val moshiCodeGen = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${SharedVersions.coroutines}"
 }
 
 object RetrofitLibraries {
@@ -88,7 +94,11 @@ object KoinLibraries {
 object TestLibraries {
     private object Versions {
         const val junit4 = "4.12"
+        const val mockk = "1.9.3"
     }
 
     const val junit4 = "junit:junit:${Versions.junit4}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${SharedVersions.coroutines}"
+    const val junitKotlin = "org.jetbrains.kotlin:kotlin-test-junit:${SharedVersions.kotlin}"
 }
