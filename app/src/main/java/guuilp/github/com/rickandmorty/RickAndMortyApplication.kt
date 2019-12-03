731e73a41e37a528479de111a0ac1915be254c50
Guilherme Pereira <guuuilp@gmail.com>
2019-12-02 21:56:52 -0300

    Extracting app koin modules to a top level val.

--- app/src/main/java/guuilp/github/com/rickandmorty/RickAndMortyApplication.kt
+++ app/src/main/java/guuilp/github/com/rickandmorty/RickAndMortyApplication.kt
@@ -8,13 +8,15 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
+val applicationModules = listOf(remoteModule, dataModule, domainModule)
+
 class RickAndMortyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidLogger()
             androidContext(this@RickAndMortyApplication)
-            modules(listOf(remoteModule, dataModule, domainModule))
+            modules(applicationModules)
         }
     }
 }
